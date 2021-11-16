465bdfcd0f1f6eaea3d1aa90a76f532b0e7244ac
Lucas Braga <lukasbraga80@gmail.com>
2021-11-16 19:43:18 -0300

    Create 06ex.py
    06 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.

---
+++ 01 - EstruturaSequencial/06Ex.py
@@ -0,0 +1,6 @@
+'''06 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.'''
+
+R = float(input('Digite o raio do circulo: '))
+A = 3.1415 * (R**2)
+
+print(f'A area do circulo é de {A:.2f}')
---
+++ 01 - EstruturaSequencial/07Ex.py
@@ -0,0 +1,6 @@
+'''07 - Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.'''
+
+lado = float(input('Digite o valor do lado do quadrado: '))
+area = lado * lado
+
+print(f'A area do qudrado é igual a {area:.2f}, o dobro da area pe igual a {area * 2:.2f}')
