f4ded8ba1a5e38db3c319650ced707f61794a557
Lucas Braga <lukasbraga80@gmail.com>
2021-11-16 19:31:12 -0300

    Create 05ex.py
    05 - Faça um Programa que converta metros para centímetros.

---
+++ 01 - EstruturaSequencial/05ex.py
@@ -0,0 +1,6 @@
+'''05 - Faça um Programa que converta metros para centímetros.'''
+
+metros = float(input('Digite um valor em metros que será convertido: '))
+cm = metros*100
+
+print(f'{metros} metros é igual a {cm} centimetros')
