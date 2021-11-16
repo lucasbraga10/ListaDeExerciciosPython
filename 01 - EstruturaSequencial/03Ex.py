fd163f33459f559c7141eeaf8ed4c062633dc30a
Lucas Braga <lukasbraga80@gmail.com>
2021-11-16 19:18:46 -0300

    Create 03Ex.py
    03 - Faça um Programa que peça dois números e imprima a soma.

---
+++ 01 - EstruturaSequencial/03Ex.py
@@ -0,0 +1,7 @@
+'''03 - Faça um Programa que peça dois números e imprima a soma.'''
+
+n1 = float(input('Digite um número: '))
+n2 = float(input('Digite outro número: '))
+
+print()
+print(f'A soma dos números digitador é {n1 + n2}')
