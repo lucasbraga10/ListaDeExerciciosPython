268ab08737e1ac9f21d8d930979f9a3d489bb8f8
Lucas Braga <lukasbraga80@gmail.com>
2021-11-16 20:32:43 -0300

    Create 08Ex.py
    08 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.

---
+++ 01 - EstruturaSequencial/08Ex.py
@@ -0,0 +1,10 @@
+import datetime
+'''08 - Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. 
+Calcule e mostre o total do seu salário no referido mês.'''
+
+valor_hora = float(input('Digite o valor da hora trabalhada: '))
+horas_trabalhadas = float(input('Digite apenas a quantidade de horas trabalhadas: '))
+minutos_trabalhados = float(input('Digite apenas a quantidade de minutos trabalhados: '))
+salario = float(((horas_trabalhadas + (minutos_trabalhados/60)) * valor_hora))
+
+print(f'O salário será de R$:{salario:.2f} reais.')
