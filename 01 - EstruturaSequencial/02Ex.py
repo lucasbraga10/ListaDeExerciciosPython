9b9c0e47f69a05f96c7f3ce57ce877940dc7d1b4
Lucas Braga <lukasbraga80@gmail.com>
2021-11-16 19:16:02 -0300

    Create 02Ex.py
    02 - Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].

---
+++ 01 - EstruturaSequencial/02Ex.py
@@ -0,0 +1,6 @@
+'''02 - Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].'''
+
+numero = float(input('Digite um número: '))
+
+print()
+print(f'O número digitado foi {numero:.2f}')
