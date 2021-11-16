7577d8db55f0c22b45df82d0493c9efeae106fd1
Lucas Braga <lukasbraga80@gmail.com>
2021-11-16 19:43:55 -0300

    Update 07Ex.py

--- 01 - EstruturaSequencial/07Ex.py
+++ 01 - EstruturaSequencial/07Ex.py
@@ -3,4 +3,5 @@
 lado = float(input('Digite o valor do lado do quadrado: '))
 area = lado * lado
 
+print()
 print(f'A area do qudrado é igual a {area:.2f}, o dobro da area pe igual a {area * 2:.2f}')
